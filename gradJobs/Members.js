06662679a23de10d6f45e402bbd5294defea3041
Josh Rogers <joshromanrogers@gmail.com>
2019-05-18 16:20:38 +0200

    members added

---
+++ gradJobs/Members.js
@@ -0,0 +1,22 @@
+const members = [
+	{
+		id: 1,
+		name: "John Doe",
+		email: "john@gmail.com",
+		status: "active"
+	},
+	{
+		id: 2,
+		name: "James Doe",
+		email: "james@gmail.com",
+		status: "inactive"
+	},
+	{
+		id: 3,
+		name: "Peter Doe",
+		email: "peter@gmail.com",
+		status: "active"
+	}
+];
+
+module.exports = members;
--- gradJobs/index.js
+++ gradJobs/index.js
@@ -1,24 +1,34 @@
-const express = require('express');
-var request = require('request');
-const path = require('path'); 
+const express = require("express");
+var request = require("request");
+const path = require("path");
+const mongoose = require("mongoose");
+const members = require("./Members");
 
 const app = express();
 
+
+// mongoose.connect("mongodb+srv://romanrogers:" + process.env.MONGO_ATLAS_PW + "@cluster0-xcfmt.mongodb.net/test?retryWrites=true", {
+// 	useNewUrlParser: true
+// });
+
+app.use("/api/members", require("./routes/api/members"));
+
+
 // SET STATIC FOLDER
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // GET API INFO FROM REED
-request.get(' https://www.reed.co.uk/api/1.0/search?keywords=graduate', {
-  'auth': {
-    'user': '417100be-8a8c-46f8-8663-ef89647a035e',
-    'pass': '',
-  }
+request.get(" https://www.reed.co.uk/api/1.0/search?keywords=graduate", {
+	"auth": {
+		"user": "417100be-8a8c-46f8-8663-ef89647a035e",
+		"pass": "",
+	}
 }, (err, res, body) => {
-    var info = JSON.parse(body);
-    var results = info.results;
-    results.forEach(result => {
-        console.log(result.jobId);
-    })
+	var info = JSON.parse(body);
+	var results = info.results;
+	results.forEach(result => {
+		// console.log(result.jobId);
+	});
 });
 
 const PORT = process.env.PORT || 5000;
