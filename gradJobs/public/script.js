9063b100aaf658eb99b727610d00dbbac27362e8
Josh Rogers <joshromanrogers@gmail.com>
2019-06-19 21:37:15 +0100

    show + hide job description onclick

--- gradJobs/public/script.js
+++ gradJobs/public/script.js
@@ -1,3 +1,16 @@
-let mainRows = document.getElementsByClassName("main-row");
+let mainRows = document.querySelectorAll(".main-row");
+let descriptionRows = document.querySelectorAll(".description-row");
 
-console.log(mainRows);
+// If user clicks on row, display the description.
+// If the description is already being displayed when clicked, hide the description.
+
+mainRows.forEach(row => {
+    row.onclick = () => {
+        if(row.nextElementSibling.style.display !== "table-row"){
+            row.nextElementSibling.style.display = "table-row";
+        }
+        else {
+            row.nextElementSibling.style.display = "none";
+        }  
+    };
+});
