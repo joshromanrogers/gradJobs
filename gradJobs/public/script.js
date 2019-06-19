3f757b29d0f5c0e2363a8666374be1cb2dbef518
Josh Rogers <joshromanrogers@gmail.com>
2019-06-19 22:03:48 +0100

    table row styling

--- gradJobs/public/script.js
+++ gradJobs/public/script.js
@@ -1,10 +1,9 @@
 let mainRows = document.querySelectorAll(".main-row");
 let descriptionRows = document.querySelectorAll(".description-row");
 
-// If user clicks on row, display the description.
-// If the description is already being displayed when clicked, hide the description.
-
 mainRows.forEach(row => {
+    // If user clicks on row, display the description.
+    // If the description is already being displayed when clicked, hide the description.
     row.onclick = () => {
         if(row.nextElementSibling.style.display !== "table-row"){
             row.nextElementSibling.style.display = "table-row";
