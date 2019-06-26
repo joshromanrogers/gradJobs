590be7924d6db255bc135300e9c451afa1925ab0
Josh Rogers <joshromanrogers@gmail.com>
2019-06-26 19:49:20 +0100

    createdAt added to schema w/ 60 day expiry

--- gradJobs/models/job.js
+++ gradJobs/models/job.js
@@ -16,6 +16,7 @@ const jobSchema = new Schema({
 	categories: { type: Array, required: false},
 	description: { type: String, required: false },
 	created: { type: String, required: true},
+	createdAt: { type: Date, default: Date.now, expires: '86400m' },
 	status: {type: String, required: true, default: 'scraped'} // 'scraped' 'not_confirmed' 'confirmed
 }, {
 	timestamps: true
