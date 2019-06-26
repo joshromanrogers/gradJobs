e77b0cfcf4048b47676443054830aef07a66f0f5
Josh Rogers <joshromanrogers@gmail.com>
2019-06-26 18:06:18 +0100

    fixed search bar for categories

--- gradJobs/api/routes/jobs.js
+++ gradJobs/api/routes/jobs.js
@@ -7,14 +7,10 @@ if(process.env.NODE_ENV !== 'production') {
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
-// schema of what a job must look like
 const Job = require("../../models/job");
-// connecting to db
 const mongoose = require("mongoose");
 var Moment = require("moment");
 const bodyParser = require("body-parser");
-
-
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
 const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
 
@@ -27,20 +23,24 @@ router.get("/", (req, res) => {
 	// run a search for term with mongoose find function
 	// if no error, pass data to index.ejs + render :D
 	if (req.query.search) {
-		const regex = new RegExp(reg.escapeRegExp(req.query.search));
+		// const regex = new RegExp(reg.escapeRegExp(req.query.search));
 		Job.find({
-		}, function (err, allJobs) {
+			categories: req.query.search,
+		}, null, {
+			sort: {
+				createdAt: -1
+			}
+		}, (err, jobs) => {
 			if (err) {
 				console.log(err);
 			} else {
 				res.render("index", {
-					data: allJobs,
+					data: jobs,
 					moment: Moment
 				});
 				return;
 			}
 		});
-	
 	} else {
 		Job.find({}, null, {
 			sort: {
@@ -50,7 +50,6 @@ router.get("/", (req, res) => {
 			if (err) {
 				console.log(err);
 			} else {
-				console.log(jobs);
 				res.render("index", {
 					data: jobs,
 					moment: Moment
@@ -58,13 +57,10 @@ router.get("/", (req, res) => {
 			}
 		});
 	}
-
 });
-
 
 // CREATE A NEW JOB
 router.post("/postJob", (req, res, next) => {
-		// create new job using the Job model that's imported from models.js
 	const newJob = new Job({
 		_id: new mongoose.Types.ObjectId(),
 		title: req.body.title,
@@ -73,14 +69,8 @@ router.post("/postJob", (req, res, next) => {
 		created: new Moment().fromNow(),
 	});
 
-	// save is provided by mongoose which can be used on mongoose models,
-	// will store in the DB
-	newJob
-		.save()
-		// if a new job is created, log + respond with 201 status + JSON message
-		// and a structured response of info about the job
+	newJob.save()
 		.then(result => {
-			console.log(result);
 			res.status(201).json({
 				message: "Handling POST request to /jobs",
 				createdJob: {
@@ -96,7 +86,6 @@ router.post("/postJob", (req, res, next) => {
 				}
 			});
 		})
-		// if job creation fails...
 		.catch(err => {
 			console.log(err);
 			console.log('failed');
@@ -115,7 +104,7 @@ router.post('/webhook', bodyParser.raw({type: 'application/json'}), (request, re
   
 	try {
 	  event = JSON.parse(request.body);
-	}
+	}	
 	catch (err) {
 	  response.status(400).send(`Webhook Error: ${err.message}`);
 	}
@@ -138,8 +127,9 @@ router.post('/webhook', bodyParser.raw({type: 'application/json'}), (request, re
   
 	// Return a response to acknowledge receipt of the event
 	response.json({received: true});
-  });
+});
 
+// JOB POSTING PAGE
 router.get("/postJob", (req, res) => {
 	fs.readFile("items.json", function(error, data) {
 		if (error) {
@@ -149,14 +139,12 @@ router.get("/postJob", (req, res) => {
 			res.render("postJob", {
 				stripePublicKey: stripePublicKey,
 				items: JSON.parse(data)
-			})
+			});
 		}
-	})
+	});
 });
 
-
 // DYNAMICALLY RETURNS ALL JOBS W/ A CATEGORY THAT IS EQUAL TO THE PARAM NAME
-// data in resultArray[0] is apssed to index.ejs
 router.get("/:name", (req, res) => {
 	let cat = req.params.name;
 
@@ -177,190 +165,5 @@ router.get("/:name", (req, res) => {
 		}
 	});
 });
-
-
-// RETURNS ALL JOBS WITH 'TECH' CATEGORY
-// data in resultArray[0] is apssed to index.ejs
-// router.get("/tech", (req, res) => {
-// 	Job.find({
-// 		categories: "tech"
-// 	}, null, {
-// 		sort: {
-// 			date: 1
-// 		}
-// 	}, (err, jobs) => {
-// 		if (err) {
-// 			console.log(err);
-// 		} else {
-// 			resultArray = [];
-// 			resultArray.push(jobs);
-// 			res.render("index", {
-// 				data: resultArray[0],
-// 				moment: Moment
-// 			});
-// 		}
-// 	});
-// });
-
-// GET ALL JOBS
-// router.get("/", (req, res, next) => {
-// 	Job.find()
-// 		// only returns the values that are selected
-// 		.select("_id title date url")
-// 		.exec()
-// 		.then(docs => {
-// 			// providing a structured response when GETing which includes what to do to
-// 			// get more information about this
-// 			const response = {
-// 				count: docs.length,
-// 				products: docs.map(doc => {
-// 					return {
-// 						_id: doc._id,
-// 						title: doc.title,
-// 						date: doc.date,
-// 						url: doc.url,
-
-// 						request: {
-// 							type: "GET",
-// 							url: "http://localhost:2000/jobs/" + doc._id
-// 						}
-// 					};
-// 				})
-// 			};
-// 			res.status(200).json(response);
-// 		})
-// 		.catch(err => {
-// 			console.log(err);
-// 			res.status(500).json({
-// 				error: err
-// 			});
-// 		});
-// });
-
-// GET SINGLE JOB 
-// (colon makes it a dynamic id )
-// router.get("/:id", (req, res, next) => {
-// 	// extract id from request
-// 	const id = req.params.id;
-// 	Job.findById(id)
-// 		// only returns the values that are selected
-// 		.select("_id title date url")
-// 		.exec()
-// 		// if it works, print the doc, respond w/ a 200 status + the documentation as JSON
-// 		.then(doc => {
-// 			console.log("From database:", doc);
-// 			if (doc) {
-// 				res.status(200).json({
-// 					product: doc,
-// 					request: {
-// 						type: "GET",
-// 						url: "http://localhost:2000/jobs/" + doc._id					}
-// 				});
-// 			} else {
-// 				res.status(404).json({
-// 					message: "no valid data found for provided ID"
-// 				});
-// 			}
-// 		})
-// 		.catch(err => {
-// 			// if it doesn't work, print the error + respond w/ a 500 status + a JSON formatted error
-// 			console.log(err);
-// 			res.status(500).json({
-// 				error: err
-// 			});
-// 		});
-
-// });
-
-// CREATE A NEW JOB
-// router.post("/", (req, res, next) => {
-
-// 	// create new job using the Job model that's imported from models.js
-// 	const newJob = new Job({
-// 		_id: new mongoose.Types.ObjectId(),
-// 		title: req.body.title,
-// 		date: req.body.date,
-// 		url: req.body.url
-// 	});
-
-// 	// save is provided by mongoose which can be used on mongoose models,
-// 	// will store in the DB
-// 	newJob
-// 		.save()
-// 		// if a new job is created, log + respond with 201 status + JSON message
-// 		// and a structured response of info about the job
-// 		.then(result => {
-// 			console.log(result);
-// 			res.status(201).json({
-// 				message: "Handling POST request to /jobs",
-// 				createdJob: {
-// 					_id: result._id,
-// 					title: result.title,
-// 					date: result.date,
-// 					url: result.url,
-
-// 					request: {
-// 						type: "POST",
-// 						url: "http://localhost:2000/jobs/" + result._id
-// 					}
-// 				}
-// 			});
-// 		})
-// 		// if job creation fails...
-// 		.catch(err => {
-// 			console.log(err);
-// 			res.status(500).json({
-// 				error: err
-// 			});
-// 		});
-
-
-// });
-
-// UPDATE A JOB
-// router.patch("/:id", (req, res, next) => {
-// 	const id = req.params.id;
-// 	const props = req.body;
-// 	Job.updateOne({
-// 		_id: id
-// 	}, props)
-// 		.exec()
-// 		.then(result => {
-// 			res.status(200).json({
-// 				message: "Job updated",
-// 				request: {
-// 					type: "GET",
-// 					url: "http://localhost:2000/jobs" + id
-// 				}
-// 			});
-// 		})
-// 		.catch(err => {
-// 			console.log(err);
-// 			res.status(500).json({
-// 				error: err
-// 			});
-// 		});
-// });
-
-// DELETE A JOB
-// router.delete("/:id", (req, res, next) => {
-// 	// extract id from request
-// 	const id = req.params.id;
-// 	Job.remove({
-// 		_id: id
-// 	})
-// 		.exec()
-// 		.then(result => {
-// 			res.status(200).json({
-// 				message: "Product deleted",
-// 			});
-// 		})
-// 		.catch(err => {
-// 			console.log(err);
-// 			res.status(500).json({
-// 				error: err
-// 			});
-// 		});
-// });
 
 module.exports = router;
